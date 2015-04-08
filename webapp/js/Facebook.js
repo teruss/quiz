3c9098b18e41351ba28f446742c0891a36f7108f
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2015-04-08 23:17:09 +0900

    Add Facebook login

---
+++ webapp/js/Facebook.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '576444712448750',
+    xfbml      : true,
+    version    : 'v2.3'
+  });
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    console.log('Logged in.');
+  }
+  else {
+    FB.login();
+  }
+});
