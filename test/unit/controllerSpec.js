460a955ca53ca1c23a8e3cf4615331e7991fb23f
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2015-04-17 00:59:59 +0900

    refactoring spec

--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -1,21 +1,22 @@
 describe('QuizCtrl', function() {
+  var scope, ctrl;
+  
   beforeEach(module('quizApp'));
 
-  it('should return 10 minutes interval if wrong answer', inject(function($controller) {
-    var scope = {},
-	ctrl = $controller('QuizCtrl', {$scope:scope});
+  beforeEach(inject(function($controller) {
+    scope = {};
+    ctrl = $controller('QuizCtrl', {$scope:scope});
+  }));
+
+  it('should return 10 minutes interval if wrong answer', function() {
     expect(scope.calcInterval(1000, 0, 0, false)).toBe(600 * 1000 * 1000 * 10);
-  }));
+  });
 
-  it('should return 1200 ticks if interval is 1000 and correct answer', inject(function($controller) {
-    var scope = {},
-	ctrl = $controller('QuizCtrl', {$scope:scope});
+  it('should return 1200 ticks if interval is 1000 and correct answer', function() {
     expect(scope.calcInterval(1000, 0, 0, true)).toBe(1200);
-  }));
+  });
 
-  it('should return 1260 ticks if interval is 1000 and now is 100 and due is 0 and correct answer', inject(function($controller) {
-    var scope = {},
-	ctrl = $controller('QuizCtrl', {$scope:scope});
+  it('should return 1260 ticks if interval is 1000 and now is 100 and due is 0 and correct answer', function() {
     expect(scope.calcInterval(1000, 0, 100, true)).toBe(1260);
-  }));
+  });
 });
