7f5d591afe2f8b662f5ca076a803bb7900761a6d
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2016-07-02 18:59:31 +0900

    Fix jquery

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config){
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-facebook/lib/angular-facebook.js',
+      'bower_components/jquery/dist/jquery.min.js',
       'js/**/*.js',
       'test/unit/**/*.js'
     ],
